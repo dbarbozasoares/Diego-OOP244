666a328d065fb968ee8b53b1c26ecc708762df14
dbarbozasoares <dbarboza.soares@gmail.com>
2024-01-25 13:56:47 -0500

    add

--- WS01/DIY/StMark.h
+++ WS01/DIY/StMark.h
@@ -12,7 +12,7 @@ Name            Date            Reason
 #ifndef SENECA_STMARK_H
 #define SENECA_STMARK_H
 namespace seneca {
-
+    const int max_cat_range = 10;
    /// <summary>
    /// Record of a mark received by a student.
    /// </summary>
@@ -21,6 +21,17 @@ namespace seneca {
       char surname[31];
       int mark;
    };
+   bool printReport(const char* filename);
+   // bubble sorting by marks in descending order
+   void sortArray(struct StMark* studentsArr);
+   // Display all student data in descending order
+   void displayStdData(struct StMark* stdArray, int);
+   // Fill up students array
+   void fillStdArray(struct StMark* stdArray, int size);
+   // stores range number 0-10: array[10] and so on...
+   void marksRange(struct StMark* studentsArr, int* numByRange, int totalStudents);
+   // Prints a graph comparing the sample values visually 
+   void printGraph(int samples[], int noOfSamples, const char* label);
 
 }
 #endif // !SENECA_STMARK_H
