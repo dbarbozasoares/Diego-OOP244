6c431dc1cb2c847b3c894c2d5c639a2de62af26b
dbarbozasoares <dbarboza.soares@gmail.com>
2024-02-22 20:39:23 -0500

    ALL DONE!

--- WS05/lab/bankaccount.cpp
+++ WS05/lab/bankaccount.cpp
@@ -1,4 +1,8 @@
-	/***********************************************************************
+// Student's name: Diego B Soares
+// ID number: 145820239
+// 02/18/2024
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.	
+/***********************************************************************
 	// OOP244 Workshop #5 Lab (part 1)
 	//
 	// File  bankaccount.cpp
@@ -43,37 +47,47 @@
 			return true;
 		}
 
+		//@param true or false
+		//@brief helper member function to control different behaves on overload functions
 		void bankAccount::setWithdrawStatus(bool value)
 		{
 			this->isWithdrawTrans = value;
 		}
 
+		//@param true or false
+		//@brief helper member function to control different behaves on overload functions
 		void bankAccount::setDepositStatus(bool value) {
 			this->isDepositTrans = value;
 		}
 
+		//@param true or false
+		//@brief helper member function to control different results on operators overload
 		void bankAccount::setFeeStatus(bool value) {
 			this->isFeeTrans = value;
 		}
 
-		// Account is open if user name string is not empty
+		// return true if current user name string is not empty
 		bool bankAccount::isOpen() const
 		{
 			return (m_userName[0] != '\0');
 		}
 
+		// return current object balance
 		bankAccount::operator double() const {
 			return this->m_funds;
 		}
 
+		// return true if current account is open and balance different than 0
 		bankAccount::operator bool() const {
 			return isOpen() && (m_funds != 0.0);
 		}
 		// ADD: Type Conversion, Unary & Binary Operators; + Additional Methods
 
+		//@return current object
+		//@brief add interest into account if it has positive balance using overload +=
 		bankAccount& bankAccount::operator++() {
 			if (this->isOpen() && this->m_funds > 0) {
-				setFeeStatus(true);
+				setFeeStatus(true); // make sure we do the right output on += overload
 				if (this->m_checking) {
 					*this += (this->m_funds * CHEQUING_INTEREST);
 				}
@@ -81,10 +95,11 @@
 					*this += (this->m_funds * SAVING_INTEREST);
 				}
 			}
-			setFeeStatus(false);
+			setFeeStatus(false); // set back to false so other overload operators work as expected
 			return *this;
 		}
-
+		//@return current object
+		//@brief calculate monthly fees depending on type of account and how many transactions on that month using overload -=
 		bankAccount& bankAccount::operator--() {
 			if (this->isOpen()) {
 				setWithdrawStatus(false);
@@ -98,62 +113,78 @@
 			return *this;
 		}
 
+		// @param value to deposit.
+		// @return true if operation successful
+		// @brief 1- If it's a fee transaction, just performs math
+		// 2- If it's other functions calling it, it doesn't count as 1 transaction
+		// 3- If it's indeed a deposit transaction, do all number 2 and counts as 1 transaction
 		bool bankAccount::operator+= (double deposit) {
 			bool checker = false;
 			if (this->isOpen()) {
-				if (isFeeTrans) {
+				if (isFeeTrans) { // on overload ++ it sets to true so this if works only if called from ++ operator
 					this->m_funds += deposit; // just do the math and doesn't count as a transaction
 				}
-				else if(!isDepositTrans){
+				else if(!isDepositTrans){ // if its called from any other operator, add deposit value into account, prints receipt but don't count as 1 transaction
 					if(deposit > 0)
 					cout << "Deposit $" << fixed << setprecision(2) << deposit << " for " << this->m_userName << endl;
-					this->m_funds += deposit; // if it's other functions calling this += overload, it just performs the math needed and doesn't count as a transaction
+					this->m_funds += deposit;
 				}
 				else if (isDepositTrans) {
-					this->m_funds += deposit;
-					this->m_monthlyTransactions++;
 					cout << "Deposit $" << fixed << setprecision(2) <<  deposit << " for " << this->m_userName << endl;
+					this->m_funds += deposit;
+					this->m_monthlyTransactions++; // if it's a deposit transaction, count as 1 transaction
 				}
 				checker = true;
 			}
 			return checker;
 		}
 
+		//@return true if operation successful
+		//@param value to withdraw
+		// @brief executes a withdraw if account is open, if it's other functions calling it, do math only
 		bool bankAccount::operator-= (double withdraw) {
 			bool validator = false;
 			if (this->isOpen()) {
-
+				setDepositStatus(false); // make sure it doesn't count as transaction on the following +=
 				if (!isWithdrawTrans) { // if its other function calling this overload -=, do the math but doesn't print and doesn't count as transaction
-					setDepositStatus(false); // make sure it doesn't count as transaction on the following +=
 					*this += (withdraw * (-1)); // returns a negative number so we subtract instead of add
 				}
-				else if (isWithdrawTrans) {
+				else if (isWithdrawTrans) { // if it's a withdraw transaction, prints statement, do math and counts as 1 transaction
 					cout << "Withdraw $" << withdraw << " for " << this->m_userName << endl;
-					isDepositTrans = false; 
 					*this += (withdraw * (-1));
 					this->m_monthlyTransactions++;
 				}
-				
 				validator = true;
 			}
-			setDepositStatus(true);
+			setDepositStatus(true); // set deposit status back to original so it doesn't interferee on += overload
 			return validator;
 		}
 
-		// compares name, balance and type of account
+		//@return true if accounts are equal
+		//@param object to compare with current object
+		//@brief compares name, balance and type of account
 		bool bankAccount::operator== (const bankAccount& tmp) const {
 			bool difference = ((this->m_funds - tmp.m_funds) < 0.001);
 			return ((strcmp(this->m_userName, tmp.m_userName) == 0) && difference && (this->m_checking == tmp.m_checking));
 		}
 
+		//@return true if current object balance is greater than param
+		//@param value to compare to current object's balance
+		//@brief compares current object's balance with double paramenter
 		bool bankAccount::operator> (double compare) const {
 			return this->m_funds > compare;
 		}
 
+		//@return true if current account's balance is less or equal to param
+		//@param value to compare to current account's balance
+		//@brief compares if current account's balance is less or equal to param
 		bool bankAccount::operator<= (double compare) const {
 			return this->m_funds <= compare;
 		}
 
+		//@return true if operation successful
+		//@param account instance to pull balance
+		//@brief performs a balance transfer from parameter account to current account
 		bool bankAccount::operator<<(bankAccount& tmp) {
 			bool validator = false;
 			if (this->isOpen() && tmp.isOpen() && tmp > 0.0) {
@@ -165,11 +196,12 @@
 				validator = true;
 				this->m_monthlyTransactions++;
 			}
-			isWithdrawTrans = true;
-			isDepositTrans = true;
+			setWithdrawStatus(true);
+			setDepositStatus(true);
 			return validator;
 		}
 
+		// display all account information,
 		void bankAccount::display(void) const {
 			char notOpen[10] = " NOT OPEN";
 			const char* accType = this->m_checking ? "Checking" : "Savings";	
@@ -190,12 +222,16 @@
 
 		// ADD: Global Helper Functions
 
-		// friend function to access is account is open
+		// friend function to access if account is open
 		bool isAccountOpen(const bankAccount& account) 
 		{
 			return account.isOpen();
 		}
 
+		//@return true if it's greater
+		//@param lhs double value to compare
+		//@param rhs account instance to compare balance
+		//@brief performs a comparison if lhs is greater than account balance
 		bool operator> (double lhs, const bankAccount& rhs) {
 			bool validator = false;
 			if (isAccountOpen(rhs)) { // using friend function to check if the account is open
@@ -204,6 +240,11 @@
 			return validator;
 		}
 
+
+		//@return true if is less or equal
+		//@param lhs double value to compare
+		//@param rhs account instance to compare balance
+		//@brief performs a comparison if lhs is less o equal than account balance
 		bool operator<= (double lhs, const bankAccount& rhs) {
 			return !(lhs > (double)rhs);
 		}
--- WS05/lab/bankaccount.h
+++ WS05/lab/bankaccount.h
@@ -1,3 +1,7 @@
+// Student's name: Diego B Soares
+// ID number: 145820239
+// 02/18/2024
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.	
 /***********************************************************************
 // OOP244 Workshop #5 Lab (part 1)
 //
@@ -37,33 +41,33 @@ namespace seneca
       bool isWithdrawTrans = true; // this variable make sure we only print client is making a withdraw
       bool isDepositTrans = true; //  this variable make sure we only print if client is making a deposit
       bool isFeeTrans = false; // it just becomes true if its inside ++ overload
-      bool isOpen() const;
+      bool isOpen() const;// return true if current user name string is not empty
    public:
-      bankAccount(const char* name = nullptr, bool checking = false);
-      bool setup(const char* name, bool checking);
-      operator double() const;
-      operator bool() const;
-      void setDepositStatus(bool);
-      void setWithdrawStatus(bool);
-      void setFeeStatus(bool);
-      bankAccount& operator++();
-      bankAccount& operator--();
-      bool operator+= (double);
-      bool operator-= (double);
-      bool operator== (const bankAccount&) const;
-      bool operator> (double) const;
-      bool operator<= (double) const;
-      bool operator<<(bankAccount&);
-      void display(void) const;
-      friend bool isAccountOpen(const bankAccount& account);
+      bankAccount(const char* name = nullptr, bool checking = false); // Default Constructor with default arguments
+      bool setup(const char* name, bool checking); // Open bank account: setup can only be done once!
+      operator double() const;// return current object balance
+      operator bool() const;// return true if current account is open and balance different than 0
+      void setDepositStatus(bool);// helper member function to control different behaves on overload functions
+      void setWithdrawStatus(bool); // helper member function to control different behaves on overload functions
+      void setFeeStatus(bool);// helper member function to control different results on operators overload
+      bankAccount& operator++();// add interest into account if it has positive balance using overload +=
+      bankAccount& operator--();// calculate monthly fees depending on type of account and how many transactions on that month using overload -=
+      bool operator+= (double); // performs a deposit into current account
+      bool operator-= (double); // performs a withdraw from current account
+      bool operator== (const bankAccount&) const; // compares name, balance and type of account
+      bool operator> (double) const; // compares current object's balance with double paramenter
+      bool operator<= (double) const;// compares if current account's balance is less or equal to param
+      bool operator<<(bankAccount&);// performs a balance transfer from parameter account to current account
+      void display(void) const;// display all account information,
+      friend bool isAccountOpen(const bankAccount& account);// friend function to access if account is open
 
       // ADD: Type Conversion, Unary, Binary Operators & Additional Methods
       // ...
    };
 
    // ADD: Global helpers
-   bool operator> (double lhs, const bankAccount& rhs);
-   bool operator<= (double lhs, const bankAccount& rhs);
+   bool operator> (double lhs, const bankAccount& rhs);// performs a comparison if lhs is greater than account balance
+   bool operator<= (double lhs, const bankAccount& rhs);// performs a comparison if lhs is less o equal than account balance
    // ...
 }
 #endif
